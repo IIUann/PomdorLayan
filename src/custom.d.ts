3dc4bbab053845f5c9b76ed631106ec6c5acc499
Layanz <iiuanz@Layanzs-MacBook-Air.local>
2026-06-01 23:08:22 +0300

    clean web version

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+declare module "meow.mp3" {
+    const src: string;
+    export default src;
+  }
+  declare module "*.mp3";
+declare module "*.gif";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.otf";
